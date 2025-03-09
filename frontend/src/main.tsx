7e6842e409482483130f4c70ce24295088e3d6bb
Dakshin Karthick S <23z312@psgtech.ac.in>
2025-03-09 19:02:20 +0530

    add route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.tsx';
+import Events from './Events.tsx'; // Import the new Events component
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/events" element={<Events />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
