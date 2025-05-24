613ca51e0c318db2a929005a46f47e66c5c60cdf
Adhish-Krishna <adhishthesak@gmail.com>
2025-05-24 20:56:25 +0530

    Added event images for gallery

--- aiconsortium/src/data/gallery.ts
+++ aiconsortium/src/data/gallery.ts
@@ -5,12 +5,38 @@ import AIweekAP04 from '/images/eventImages/AIWeekActivityPlan04.JPG?url';
 import AIweekAP05 from '/images/eventImages/AIWeekActivityPlan05.JPG?url';
 import AIweekAP06 from '/images/eventImages/AIWeekActivityPlan06.JPG?url';
 
+// Ai Consortium Community Meet
 import screenshot211846 from '/images/eventImages/Screenshot_20250327_211846_LinkedIn.jpg';
 import screenshot211908 from '/images/eventImages/Screenshot_20250327_211908_LinkedIn.jpg';
 import screenshot212041 from '/images/eventImages/Screenshot_20250327_212041_LinkedIn.jpg';
 import screenshot212110 from '/images/eventImages/Screenshot_20250327_212110_LinkedIn.jpg';
 import screenshot212145 from '/images/eventImages/Screenshot_20250327_212145_LinkedIn.jpg';
 import screenshot212128 from "/images/eventImages/Screenshot_20250327_212128_LinkedIn.jpg";
+
+//AI Week Webinar on "AI Adoption in India Opportunities and Security Challenges"
+import AIWeekWebinar01 from '/images/AI WEEK _Webinar on “AI Adoption in India_ Opportunities & Security Challenges_ 15 April 2025/WhatsApp Image 2025-04-15 at 3.06.39 PM.jpeg';
+import AIWeekWebinar02 from '/images/AI WEEK _Webinar on “AI Adoption in India_ Opportunities & Security Challenges_ 15 April 2025/WhatsApp Image 2025-04-15 at 3.07.19 PM.jpeg';
+import AIWeekWebinar03 from '/images/AI WEEK _Webinar on “AI Adoption in India_ Opportunities & Security Challenges_ 15 April 2025/WhatsApp Image 2025-04-15 at 3.08.03 PM.jpeg';
+import AIWeekWebinar04 from '/images/AI WEEK _Webinar on “AI Adoption in India_ Opportunities & Security Challenges_ 15 April 2025/WhatsApp Image 2025-04-15 at 3.14.29 PM.jpeg';
+
+// AI Week Ai in prodcution engineering webinar
+import AIWeekPEWebinar02 from '/images/AI WEEK_AI IN PRODUCTION ENGINEERING_Webinar_04 April, 2025/WhatsApp Image 2025-04-04 at 12.04.03 PM.jpeg';
+import AIWeekPEWebinar03 from '/images/AI WEEK_AI IN PRODUCTION ENGINEERING_Webinar_04 April, 2025/WhatsApp Image 2025-04-04 at 12.04.04 PM.jpeg';
+import AIWeekPEWebinar04 from '/images/AI WEEK_AI IN PRODUCTION ENGINEERING_Webinar_04 April, 2025/WhatsApp Image 2025-04-04 at 12.14.09 PM.jpeg';
+
+//AI Week in Robotics and Automation
+import AIWeekRA01 from '/images/AI WEEK_AI in Robotics and Automation 01 APRIL 2025/PHOTO-2025-04-01-10-27-05.jpg';
+import AIWeekRA02 from '/images/AI WEEK_AI in Robotics and Automation 01 APRIL 2025/PHOTO-2025-04-01-10-27-06.jpg';
+
+//AI week Panel discussion
+import AIWeekPD01 from '/images/AI WEEK_Panel Discussion_ 03 April 2025/13_page-0001.jpg';
+import AIWeekPD02 from '/images/AI WEEK_Panel Discussion_ 03 April 2025/23_page-0001.jpg';
+import AIWeekPD03 from '/images/AI WEEK_Panel Discussion_ 03 April 2025/WhatsApp Image 2025-04-04 at 12.42.25 PM.jpeg';
+import AIWeekPD04 from '/images/AI WEEK_Panel Discussion_ 03 April 2025/WhatsApp Image 2025-04-04 at 12.42.26 PM (1).jpeg';
+import AIWeekPD05 from '/images/AI WEEK_Panel Discussion_ 03 April 2025/IMG_8529.JPG?url';
+import AIWeekPD06 from '/images/AI WEEK_Panel Discussion_ 03 April 2025/IMG_8542.JPG?url';
+import AIWeekPD07 from '/images/AI WEEK_Panel Discussion_ 03 April 2025/IMG_8546.JPG?url';
+import AIWeekPD08 from '/images/AI WEEK_Panel Discussion_ 03 April 2025/IMG_8554.JPG?url';
 
 export interface Gallery{
     id: number,
@@ -66,7 +92,73 @@ const gallery: Gallery[] = [
         id: 12,
         image: screenshot212145,
     },
-
+    {
+        id:13,
+        image: AIWeekWebinar01
+    },
+    {
+        id:14,
+        image: AIWeekWebinar02
+    },
+    {
+        id:15,
+        image: AIWeekWebinar03
+    },    {
+        id:16,
+        image: AIWeekWebinar04
+    },
+    {
+        id: 17,
+        image: AIWeekPEWebinar02
+    },
+    {
+        id: 18,
+        image: AIWeekPEWebinar03
+    },
+    {
+        id: 19,
+        image: AIWeekPEWebinar04
+    },
+    {
+        id: 20,
+        image: AIWeekRA01
+    },
+    {
+        id: 21,
+        image: AIWeekRA02
+    },
+    {
+        id: 22,
+        image: AIWeekPD01
+    },
+    {
+        id: 23,
+        image: AIWeekPD02
+    },
+    {
+        id: 24,
+        image: AIWeekPD03
+    },
+    {
+        id: 25,
+        image: AIWeekPD04
+    },
+    {
+        id: 26,
+        image: AIWeekPD05
+    },
+    {
+        id: 27,
+        image: AIWeekPD06
+    },
+    {
+        id: 28,
+        image: AIWeekPD07
+    },
+    {
+        id: 29,
+        image: AIWeekPD08
+    },
 ]
 
 export {gallery};
